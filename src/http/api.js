9a1b2423b96763ee628fa5de941b45b5e61ef76e
Inaam Ur Rehman <inaamurrehman343@gmail.com>
2024-05-06 21:59:08 +0500

    First commit

--- src/http/api.js
+++ src/http/api.js
@@ -2,7 +2,7 @@ import { useAuthStore } from "@/store";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hostel-management-system-backend.onrender.com",
+  baseURL: "https://hostel-management-system-backend.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${useAuthStore.getState().user?.token || ""}`,
